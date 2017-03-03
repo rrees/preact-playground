126f305f31776a9943dc69689aab904d6a79892c
Robert Rees <shudderfix@gmail.com>
2017-03-03 22:37:03 +0000

    Creates a working entry system

--- cya/index.js
+++ cya/index.js
@@ -4,11 +4,14 @@ const { Component, h, render } = window.preact;
 class App extends Component {
 
 	render(props, state) {
+		const component = this;
+		console.log(state);
 		return (
 			h('div', {id: 'app'},
 				h('h1', null, state.adventureName),
 				h('h2', null, state.currentEntry.name),
-				h('p', null, state.currentEntry.description)
+				h('p', null, state.currentEntry.description),
+				state.currentEntry.actions.map((action) => h('p', {}, h('a', {onclick: (e) => component.setState({currentEntry: component.entries[action.nextEntry]})}, action.description)))
 			)
 		);
 	}
@@ -16,21 +19,34 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {
-			adventureName: "The Haunted House",
-			currentEntry: undefined,
-			entries: {
-				1: {
-					name: "The Grounds",
-					description: `A dark path winds through an overgrown garden to the veranda of a grand house`,
-					actions: [
-						
-					]
-				}
+		this.entries = {
+			1: {
+				name: "The grounds",
+				description: `A dark path winds through an overgrown garden to the veranda of a grand house`,
+				actions: [
+					{
+						description: "Walk onto the veranda",
+						nextEntry: 2
+					}
+				]
+			},
+			2: {
+				name: "The veranda",
+				description: `Paint peels from the wood of the veranda revealing black rot in the wood beneath.`,
+				actions: [
+					{
+						description: "Enter the overgrown grounds surrounding the house",
+						nextEntry: 1
+					}
+				]
 			}
 		};
 
-		this.state.currentEntry =  this.state.entries[1];
+
+		this.state = {
+			adventureName: "The Haunted House",
+			currentEntry: this.entries[1]
+		};
 	}
 }
 
